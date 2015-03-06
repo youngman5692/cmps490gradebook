1734b6df6e00d49c1c60120b83cb2f40197525f7
youngman5692 <youngman5692@yahoo.com>
2015-03-05 17:51:38 -0800

    front page inc

---
+++ gradebook/MyClasses.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MyClasses.aspx.cs" Inherits="gradebook.MyClasses" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>My Classes</h2>
+    <asp:Label ID="loggedIn" runat="server" Text="Login to See Your Current Classes!" Visible="true"></asp:Label>
+
+    <asp:UpdatePanel ID="studentPanel" runat="server" UpdateMode="Always">
+        <ContentTemplate>
+            <asp:GridView ID="studentCourseGridView" runat="server" autogeneratecolumns="false" CssClass="table table-striped table-bordered table-condensed">
+                <Columns>
+                    <asp:BoundField DataField="CourseNumber" HeaderText="Course" />
+                    <asp:BoundField DataField="Description" HeaderText="Course Description" />
+                </Columns>
+            </asp:GridView>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+    <asp:UpdatePanel ID="teacherPanel" runat="server" UpdateMode="Always">
+        <ContentTemplate>
+            <asp:GridView ID="teacherCourseGridView" runat="server" autogeneratecolumns="false" CssClass="table table-striped table-bordered table-condensed">
+                <Columns>
+                    <asp:BoundField DataField="CourseNumber" HeaderText="Course" />
+                    <asp:BoundField DataField="Description" HeaderText="Course Description" />
+                </Columns>
+            </asp:GridView>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+
+</asp:Content>
---
+++ gradebook/MyClasses.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using gradebook.DAL2;
+
+namespace gradebook
+{
+    public partial class MyClasses : System.Web.UI.Page
+    {
+        string email;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (User.Identity.IsAuthenticated)
+                {
+                    email = User.Identity.Name;
+                    checkRole();
+                    loggedIn.Visible = false;
+                }
+                else
+                    loggedIn.Visible = true;
+            }
+        }
+
+        protected void studentCourseGridViewFill()
+        {
+            using (var context = new GradebookDataEntities())
+            {
+                var l2equery = from s in context.Students where s.Email == email select s.Courses;
+                var courses = l2equery.FirstOrDefault();
+
+                studentCourseGridView.DataSource = courses;
+                studentCourseGridView.DataBind();
+
+            }
+        }
+
+        protected void teacherCourseGridViewFill()
+        {
+            using (var context = new GradebookDataEntities())
+            {
+                var l2equery = (from t in context.Teachers where t.Email == email select t.Courses).FirstOrDefault();
+
+                teacherCourseGridView.DataSource = l2equery;
+                teacherCourseGridView.DataBind();
+            }
+        }
+
+        protected void checkRole()
+        {
+            using (var context = new GradebookDataEntities())
+            {
+                var l2equery = (from s in context.Students where s.Email == email select s).FirstOrDefault();
+
+                if (l2equery != null)
+                {
+                    studentPanel.Visible = true;
+                    teacherPanel.Visible = false;
+                    studentCourseGridViewFill();
+                }
+                else
+                {
+                    teacherPanel.Visible = true;
+                    studentPanel.Visible = false;
+                    teacherCourseGridViewFill();
+                }
+            }
+        }
+    }
+}
